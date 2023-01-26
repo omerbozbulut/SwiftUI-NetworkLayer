76399eadcb457cfe892315b05bb0542d4779384d
Ömer Faruk Bozbulut <omerbzblt613@gmail.com>
2023-01-26 14:34:19 +0300

    init

---
+++ NetworkLayerExample/App/NetworkLayerExample.swift
@@ -0,0 +1,17 @@
+//
+//  ApiClientWithAssociatedTypesApp.swift
+//  ApiClientWithAssociatedTypes
+//
+//  Created by Ömer Bozbulut on 25.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkLayerExample: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
